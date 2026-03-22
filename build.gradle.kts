195d9240594b65ab61b315e8a621d7c0ee34dd24
✿ Fleur de Blue <135421389+Xylphy@users.noreply.github.com>
2026-03-22 19:00:02 +0800

    feat: Removal of JavaFx in gradle

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,11 +2,9 @@ plugins {
     java
     application
     id("org.javamodularity.moduleplugin") version "1.8.15"
-    id("org.openjfx.javafxplugin") version "0.0.13"
     id("org.beryx.jlink") version "2.25.0"
 }
 
-group = "org.example"
 version = "1.0-SNAPSHOT"
 
 repositories {
@@ -26,45 +24,22 @@ tasks.withType<JavaCompile> {
 }
 
 application {
-    mainModule.set("org.example.terminal228am")
-    mainClass.set("org.example.terminal228am.MainMenuApplication")
+    mainClass.set("main.Main")
 }
 
-javafx {
-    version = "21.0.6"
-    modules = listOf("javafx.controls", "javafx.fxml", "javafx.web", "javafx.swing", "javafx.media")
+tasks.register<JavaExec>("runApp") {
+    group = "application"
+    classpath = sourceSets["main"].runtimeClasspath
+    mainClass.set("main.Main")
+    standardInput = System.`in`
 }
 
 dependencies {
     implementation("org.jetbrains.kotlin:kotlin-stdlib:2.2.21")
-    implementation("org.controlsfx:controlsfx:11.2.1")
-    implementation("com.dlsc.formsfx:formsfx-core:11.6.0") {
-        exclude(group = "org.openjfx")
-    }
-    implementation("net.synedra:validatorfx:0.6.1") {
-        exclude(group = "org.openjfx")
-    }
-    implementation("org.kordamp.ikonli:ikonli-javafx:12.3.1")
-    implementation("org.kordamp.bootstrapfx:bootstrapfx-core:0.4.0")
-    implementation("eu.hansolo:tilesfx:21.0.9") {
-        exclude(group = "org.openjfx")
-    }
-    implementation("com.github.almasb:fxgl:17.3") {
-        exclude(group = "org.openjfx")
-        exclude(group = "org.jetbrains.kotlin")
-    }
     testImplementation("org.junit.jupiter:junit-jupiter-api:${junitVersion}")
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:${junitVersion}")
 }
 
 tasks.withType<Test> {
     useJUnitPlatform()
-}
-
-jlink {
-    imageZip.set(layout.buildDirectory.file("/distributions/app-${javafx.platform.classifier}.zip"))
-    options.set(listOf("--strip-debug", "--compress", "2", "--no-header-files", "--no-man-pages"))
-    launcher {
-        name = "app"
-    }
 }
