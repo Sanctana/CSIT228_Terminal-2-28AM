a22a0728eeb062491229627740c7737d001c70dd
drew5 <drew50028@gmail.com>
2026-04-26 11:04:32 +0800

    New Enemy: Brighteyes

--- src/main/java/Inventory/Defibrillator.java
+++ src/main/java/Inventory/Defibrillator.java
@@ -1,14 +1,15 @@
 package Inventory;
-
+import java.util.Random;
 import entity.Player;
 
 public class Defibrillator extends Item {
+    Random random = new Random();
     public Defibrillator() {
         super("Defibrillator", "Stabilizes player's heart rate between 60 and 80 bpm.");
     }
 
     @Override
     public void use(Player player) {
-        player.heartRate = 70;
+        player.heartRate = random.nextInt(70,81);
     }
 }
