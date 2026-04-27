ec9c094bed8fe16f13944312f2980eeb38ac95e4
iSaint <188669077+Sanctana@users.noreply.github.com>
2026-04-27 16:24:48 +0800

    Map: Finalizing Floor 3 Layout, Partial Design

--- src/main/java/tile/Map.java
+++ src/main/java/tile/Map.java
@@ -75,16 +75,90 @@ public abstract class Map {
         setup("UpperBed", TileType.WALKABLE); // 31
         setup("lowerBed", TileType.WALKABLE); // 32
 
-        setup("upperBedV2", TileType.WALKABLE); // 33
-        setup("lowerBedV2", TileType.WALKABLE); // 34
+        setup("UpperBedVerticalWall", TileType.COLLISION_TILE); // 33
+        setup("UpperBedBottom", TileType.WALKABLE); // 34
 
-        setup("blackTiles", TileType.SPAWN_POINT); // 35
+        setup("SpawnPoint", TileType.SPAWN_POINT); // 35
         setup("blackTiles", TileType.TO_NEXT_MAP); // 36
         setup("blackTilesFlower", TileType.COLLISION_TILE); // 37
         setup("LeftDoorHor", TileType.COLLISION_TILE); // 38
         setup("RightDoorHor", TileType.COLLISION_TILE); // 39
         setup("blackTiles", TileType.TO_PREVIOUS_MAP); // 40
         setup("floorTileBrighter", TileType.BATTLE_TRIGGER); // 41
+
+        setup("CabinetTopVerticalWall", TileType.COLLISION_TILE); // 42
+        setup("CabinetBottom", TileType.COLLISION_TILE); // 43
+        setup("chartAnatomy1", TileType.COLLISION_TILE); // 44
+        setup("chartAnatomy2", TileType.COLLISION_TILE); // 45
+        setup("tableMedicine1", TileType.WALKABLE); // 46
+        setup("tableMedicine2", TileType.COLLISION_TILE); // 47
+        setup("tableMonitor1", TileType.WALKABLE); // 48
+        setup("tableMonitor2", TileType.COLLISION_TILE); // 49
+        setup("flowerV1a", TileType.WALKABLE); //50
+        setup("flowerV1b", TileType.COLLISION_TILE); //51
+
+        setup("LeftArrow", TileType.COLLISION_TILE); //52
+        setup("RightArrow", TileType.COLLISION_TILE); //53
+
+        setup("tileBlood1", TileType.WALKABLE); //54
+        setup("tileBlood2", TileType.WALKABLE); // 55
+        setup("tileBlood3", TileType.WALKABLE); // 56
+
+        setup("UpperBenchTopLeft", TileType.COLLISION_TILE); //57
+        setup("UpperBenchBottomLeft", TileType.WALKABLE); // 58
+        setup("UpperBenchTopRight", TileType.COLLISION_TILE); //59
+        setup("UpperBenchBottomRight", TileType.WALKABLE); //60
+
+        setup("SpawnPointDoor", TileType.WALKABLE); // 61
+
+        setup("LeftBenchTopLeft", TileType.COLLISION_TILE); // 62
+        setup("LeftBenchBottomLeft", TileType.WALKABLE); // 63
+        setup("LeftBenchTopRight", TileType.COLLISION_TILE); //64
+        setup("LeftBenchBottomRight", TileType.WALKABLE); //65
+
+        setup("0021BlackV1", TileType.COLLISION_TILE); //66
+        setup("0021BlackV2", TileType.COLLISION_TILE); //67
+        setup("0021BlackV3", TileType.COLLISION_TILE); //68
+        setup("0021BlackV4", TileType.COLLISION_TILE); //69
+
+        setup("blackTilesWallHorBlackV1", TileType.COLLISION_TILE); // 70
+        setup("blackTilesWallHorBlackV2", TileType.COLLISION_TILE); // 71
+        setup("blackTilesWallVertBlackV1", TileType.COLLISION_TILE); // 72
+        setup("blackTilesWallVertBlackV2", TileType.COLLISION_TILE); // 73
+
+        setup("upperCornerV1", TileType.COLLISION_TILE); //74
+        setup("upperCornerV2", TileType.COLLISION_TILE); //75
+
+        setup("bottomCornerV1", TileType.COLLISION_TILE); //76
+        setup("bottomCornerV2", TileType.COLLISION_TILE); //77
+
+        setup("blackTile", TileType.COLLISION_TILE); // 78
+
+        setup("0021BlackV5", TileType.COLLISION_TILE); //79
+        setup("upperCornerV3", TileType.COLLISION_TILE); //80
+        setup("upperCornerV4", TileType.COLLISION_TILE); //81
+
+        setup("BottomBenchTopLeft", TileType.COLLISION_TILE); // 82
+        setup("BottomBenchBottomLeft", TileType.WALKABLE); // 83
+        setup("BottomBenchTopRight", TileType.COLLISION_TILE); //84
+        setup("BottomBenchBottomRight", TileType.WALKABLE); //85
+
+        setup("DoorHorV1", TileType.COLLISION_TILE); //86
+        setup("DoorHorV2", TileType.COLLISION_TILE); //87
+        setup("DoorHorV3", TileType.COLLISION_TILE); //88
+        setup("DoorHorV4", TileType.COLLISION_TILE); //89
+
+        setup("DoorSingleV1", TileType.WALKABLE); //90
+        setup("DoorSingleV2", TileType.WALKABLE); //91
+        setup("DoorSingleV3", TileType.WALKABLE); //92
+        setup("DoorSingleV4", TileType.WALKABLE); //93
+
+        setup("0021BlackV7", TileType.COLLISION_TILE); //94
+        setup("0021BlackV8", TileType.COLLISION_TILE); //95
+        setup("0021BlackV9", TileType.COLLISION_TILE); //96
+        setup("upperCornerV5", TileType.COLLISION_TILE); //97
+        setup("upperCornerV6", TileType.COLLISION_TILE); //98
+        setup("0021BlackV6", TileType.COLLISION_TILE); //99
     }
 
     public void setup(String imageName, TileType tileType) {
